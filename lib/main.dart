0ed1a2310360fe55b2b009ed15b7a3cc65b0cbae
Hevelise - Liz <liz.souza.dias@gmail.com>
2024-07-05 16:27:22 -0300

    feat: leyout pronto.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -14,10 +13,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: "Jersey15",
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade900),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'BUSCA CEP 📍'),
+      home: const MyHomePage(title: 'BUSCA CEP'),
     );
   }
 }
@@ -35,35 +35,60 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(widget.title),
-      ),
-  body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            colors: [ 
-              Colors.white,
-               Color(0xFF4be0ef),
-               Color(0xFF4be0ef),
-              ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70.0), // Altura desejada da AppBar
+        child: AppBar(
+          backgroundColor: Colors.white,
+          title: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 25.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    widget.title,
+                    style: const TextStyle(
+                      fontSize: 50,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  const Icon(
+                    Icons.location_on,
+                    color: Colors.black,
+                    size: 35,
+                  ),
+                ],
+              ),
             ),
           ),
-   child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                width: 250,
-                height: 250,
-                child: Image.asset('assets/mapa.gif'),
-              ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white,
+              Color(0xFF4be0ef),
+              Color(0xFF4be0ef),
             ],
           ),
         ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: Image.asset('assets/mapa.gif'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
