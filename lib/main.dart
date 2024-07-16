f1f3bd877258209953784279a7d6d5ca9cec2ad0
Hevelise - Liz <liz.souza.dias@gmail.com>
2024-07-16 17:13:37 -0300

    style: att.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_busca_cep/pages/home_page.dart';
-
 
 void main() {
   runApp(const MyApp());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,199 +15,33 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _MyHomePageState();
 }
 
-// class _MyHomePageState extends State<HomePage> {
-//   TextEditingController controllerCep = TextEditingController();
-
-//   //focusNode para o campo de texto
-//   FocusNode cepFocusNode = FocusNode();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     //adiciona um listener para quando o campo de texto receber foco
-//     //um listener é usado para monitorar mudanças de estado
-//     cepFocusNode.addListener(() {});
-//   }
-
-//   @override
-//   void dispose() {
-//     //dispose do FocusNode quando não for mais necessário
-//     cepFocusNode.dispose();
-//     super.dispose();
-//   }
-
-//   var cep = CepModel();
-//   final controller = CepController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: GestureDetector(
-//         onTap: () {
-//           FocusScope.of(context)
-//               .unfocus(); //remove o foco do campo de texto ao tocar em qualquer lugar da tela
-//         },
-//         child: Container(
-//           width: double.infinity,
-//           height: double.infinity,
-//           decoration: const BoxDecoration(
-//             gradient: LinearGradient(
-//               begin: Alignment.topCenter,
-//               end: Alignment.bottomCenter,
-//               colors: [
-//                 AppColors.primaryColor,
-//                 AppColors.primaryColor,
-//                 AppColors.secondaryColor,
-//               ],
-//             ),
-//           ),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               Expanded(
-//                 child: ListView(children: [
-//                   Column(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: <Widget>[
-//                       Center(
-//                         child: Padding(
-//                           padding: const EdgeInsets.only(bottom: 0),
-//                           child: SizedBox(
-//                             width: 330,
-//                             height: 140,
-//                             child: Image.asset('assets/images/placa.png'),
-//                           ),
-//                         ),
-//                       ),
-//                       Padding(
-//                         padding: const EdgeInsets.only(left: 82, right: 82),
-//                         child: TextFormField(
-//                           keyboardType: TextInputType.number,
-//                           controller: controllerCep,
-//                           focusNode: cepFocusNode,
-//                           onTap: () {
-//                             //limpa os resultados da busca anterior ao receber foco
-//                           },
-//                           decoration: const InputDecoration(
-//                             labelText: 'DIGITE SEU CEP',
-//                             labelStyle: TextStyle(fontSize: 22.0),
-//                             contentPadding: EdgeInsets.symmetric(
-//                                 vertical: 15.0, horizontal: 22.0),
-//                             border: OutlineInputBorder(
-//                               borderRadius:
-//                                   BorderRadius.all(Radius.circular(6)),
-//                             ),
-//                           ),
-//                           style: const TextStyle(fontSize: 22.0),
-//                         ),
-//                       ),
-//                       const SizedBox(height: 18),
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           ElevatedButton(
-//                             onPressed: () async {
-//                               //verifica se o CEP tem 8 números
-//                               if (controllerCep.text.length == 8 &&
-//                                   int.tryParse(controllerCep.text) != null) {
-//                                 cep = await controller
-//                                     .searchCep(controllerCep.text);
-//                               } else {
-//                                 //snackbar informando ao usuário sobre o formato incorreto do CEP
-//                                 ScaffoldMessenger.of(context).showSnackBar(
-//                                   const SnackBar(
-//                                     content: Text(
-//                                         'Digite um CEP válido com 8 dígitos.'),
-//                                   ),
-//                                 );
-//                               }
-//                             },
-//                             style: ElevatedButton.styleFrom(
-//                               padding: const EdgeInsets.symmetric(
-//                                   horizontal: 20, vertical: 10),
-//                               elevation: 6, //sombra no botão
-//                               shadowColor: Colors.black,
-//                               backgroundColor: Colors.grey.shade800,
-//                               shape: RoundedRectangleBorder(
-//                                 borderRadius: BorderRadius.circular(6),
-//                               ),
-//                             ),
-//                             child: const Row(
-//                               children: [
-//                                 Text(
-//                                   'PESQUISAR',
-//                                   style: TextStyle(
-//                                     fontSize: 22,
-//                                     color: AppColors.primaryColor,
-//                                   ),
-//                                 ),
-//                                 SizedBox(width: 8),
-//                                 Icon(Icons.search,
-//                                     color: AppColors.primaryColor),
-//                               ],
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                       Padding(
-//                         padding:
-//                             const EdgeInsets.only(left: 75, right: 75, top: 20),
-//                         child: ListenableBuilder(
-//                           //escuta o que tá sendo passado no controller
-//                           listenable: controller,
-//                           builder: (context, child) {
-//                             if (controller.isLoading == true) {
-//                               return const CircularProgressIndicator();
-//                             }
-
-//                             if (controller.isError == true) {
-//                               return MyText(
-//                                 text:
-//                                     'CEP inexistente, digite novamente um CEP válido.',
-//                                 fontSize: 20,
-//                               );
-//                             }
-//                             return MyText(
-//                                 text:
-//                                     '${cep.cep}, ${cep.estado}, ${cep.cidade}, ${cep.bairro}, ${cep.rua}.');
-//                           },
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ]),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.only(bottom: 8, top: 8),
-//                 child: SizedBox(
-//                   width: 280,
-//                   height: 180,
-//                   child: Image.asset('assets/images/mapa.gif'),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class _MyHomePageState extends State<HomePage> {
+  // Controlador de texto para o campo de entrada do CEP
   TextEditingController controllerCep = TextEditingController();
+
+  // FocusNode para controlar o foco do campo de entrada
   FocusNode cepFocusNode = FocusNode();
+
+  // Modelo para armazenar os dados do CEP
   var cep = CepModel();
+
+  // Controlador para buscar o CEP
   final controller = CepController();
+
+  // Flag para controlar a exibição da mensagem inicial
   bool initialMessage = true;
 
   @override
   void initState() {
     super.initState();
+
+    // Listener para detectar quando o campo de entrada ganha foco
     cepFocusNode.addListener(() {
       if (cepFocusNode.hasFocus) {
         setState(() {
-          initialMessage = true; //mostra a mensagem inicial quando o imput estiver em foco
-          cep = CepModel(); //limpa os dados da busca anterior
+          initialMessage =
+              true; // Mostra a mensagem inicial quando o campo de entrada estiver em foco
+          cep = CepModel(); // Limpa os dados da busca anterior
         });
       }
     });
@@ -215,6 +49,7 @@ class _MyHomePageState extends State<HomePage> {
 
   @override
   void dispose() {
+    // Dispose do FocusNode para liberar recursos
     cepFocusNode.dispose();
     super.dispose();
   }
@@ -224,7 +59,8 @@ class _MyHomePageState extends State<HomePage> {
     return Scaffold(
       body: GestureDetector(
         onTap: () {
-          FocusScope.of(context).unfocus();
+          FocusScope.of(context)
+              .unfocus(); // Remove o foco do campo de entrada ao tocar em qualquer lugar da tela
         },
         child: Container(
           width: double.infinity,
@@ -255,22 +91,32 @@ class _MyHomePageState extends State<HomePage> {
                             child: SizedBox(
                               width: 330,
                               height: 140,
-                              child: Image.asset('assets/images/placa.png'),
+                              child: Image.asset(
+                                  'assets/images/placa.png'), // Imagem de cabeçalho
                             ),
                           ),
                         ),
                         Padding(
                           padding: const EdgeInsets.only(left: 82, right: 82),
                           child: TextFormField(
-                            keyboardType: TextInputType.number,
-                            controller: controllerCep,
-                            focusNode: cepFocusNode,
+                            keyboardType: TextInputType
+                                .number, // Tipo de teclado numérico
+                            controller:
+                                controllerCep, // Controlador do campo de entrada
+                            focusNode:
+                                cepFocusNode, // FocusNode do campo de entrada
                             onTap: () {
                               setState(() {
-                                controllerCep.clear(); //limpa o texto do imput
-                                controller.isError = false; //encerra o estado de erro no controlador
-                                initialMessage = true; //mostra a mensagem inicial
-                                cep = CepModel(); //limpa os dados da busca anterior
+                                controllerCep
+                                    .clear(); // Limpa o texto do campo de entrada
+                                controller.isError =
+                                    false; // Reseta o estado de erro no controlador
+                                initialMessage =
+                                    true; // Mostra a mensagem inicial
+                                cep =
+                                    CepModel(); // Limpa os dados da busca anterior
+                                FocusScope.of(context)
+                                    .requestFocus(cepFocusNode); // Foca no campo de entrada para mostrar o teclado
                               });
                             },
                             decoration: const InputDecoration(
@@ -292,20 +138,27 @@ class _MyHomePageState extends State<HomePage> {
                           children: [
                             ElevatedButton(
                               onPressed: () async {
+                                FocusScope.of(context)
+                                    .unfocus(); // Remove o foco do campo de entrada para ocultar o teclado
                                 if (controllerCep.text.length == 8 &&
                                     int.tryParse(controllerCep.text) != null) {
                                   try {
-                                    cep = await controller.searchCep(controllerCep.text);
+                                    cep = await controller
+                                        .searchCep(controllerCep.text);
                                     setState(() {
-                                      initialMessage = false; //oculta a mensagem inicial após a pesquisa
+                                      initialMessage =
+                                          false; // Oculta a mensagem inicial após a pesquisa
                                     });
                                   } catch (e) {
-                                    //o erro será tratado no controller
+                                    // O erro será tratado no controller
                                   }
                                 } else {
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     const SnackBar(
-                                      content: Text('Digite um CEP válido com 8 dígitos.'),
+                                      content: MyText(
+                                          text:
+                                              'Digite um CEP válido com 8 dígitos.',
+                                          fontSize: 13),
                                     ),
                                   );
                                 }
@@ -330,38 +183,41 @@ class _MyHomePageState extends State<HomePage> {
                                     ),
                                   ),
                                   SizedBox(width: 8),
-                                  Icon(Icons.search, color: AppColors.primaryColor),
+                                  Icon(Icons.search,
+                                      color: AppColors.primaryColor),
                                 ],
                               ),
                             ),
                           ],
                         ),
                         Padding(
-                          padding:
-                              const EdgeInsets.only(left: 75, right: 75, top: 20),
+                          padding: const EdgeInsets.only(
+                              left: 44, right: 44, top: 20),
                           child: AnimatedBuilder(
                             animation: controller,
                             builder: (context, child) {
                               if (controller.isLoading) {
-                                return const CircularProgressIndicator();
+                                return const CircularProgressIndicator(); // Mostra o indicador de carregamento se estiver carregando
                               }
 
                               if (controller.isError) {
                                 return const MyText(
-                                  text: 'CEP inexistente, digite um CEP válido.',
+                                  text:
+                                      'CEP inexistente, digite um CEP válido.', // Mostra a mensagem de erro se houver um erro
                                   fontSize: 20,
                                 );
                               }
 
                               if (initialMessage) {
                                 return const MyText(
-                                  text: '',
+                                  text: '', // Mostra a mensagem inicial
                                   fontSize: 20,
                                 );
                               }
 
                               return MyText(
-                                text: '${cep.cep}, ${cep.estado}, ${cep.cidade}, ${cep.bairro}, ${cep.rua}.',
+                                text:
+                                    '${cep.cep}, ${cep.estado}, ${cep.cidade}, ${cep.bairro}, ${cep.rua}.', // Mostra os dados do CEP pesquisado
                               );
                             },
                           ),
@@ -376,7 +232,8 @@ class _MyHomePageState extends State<HomePage> {
                 child: SizedBox(
                   width: 280,
                   height: 180,
-                  child: Image.asset('assets/images/mapa.gif'),
+                  child:
+                      Image.asset('assets/images/mapa.gif'), 
                 ),
               ),
             ],
